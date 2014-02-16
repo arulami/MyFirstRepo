639291cf911aad65adb48a30e2e4d8ca190b68fe
Jonathan <Jonathan@VAIO>
2014-02-16 19:12:58 +0530

    Calendar Test

---
+++ CollectionExamples/src/com/CalendarEx.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarEx {
+
+	public static void main(String[] args) {
+		
+		
+		Calendar cal = Calendar.getInstance();
+		
+		Date d = cal.getTime();
+		
+		System.out.println(d);
+
+	}
+
+}
